3040548ef80b3437b522f21e5e01ce24cafde69a
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-03 10:48:43 +0600

    added routes for login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get '/login', to: 'users#login'
+  get '/logout', to: 'users#logout'
+  post '/signin', to: 'users#signin'
+  get '/suggestions', to: 'articles#suggestions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
