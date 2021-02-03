33908550fa2e51106efd0b25c901c71d4e6681b6
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-03 17:20:32 +0600

    added main section

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,37 @@
+module ArticlesHelper
+  def show_articles(category)
+    article = category.most_recent_article[0]
+    render partial: 'no-article', locals: {} if article.blank?
+    render partial: 'article', locals: { article: article, category: category } unless article.blank?
+  end
+
+  def article_owner?(article)
+    return 'd-flex' if !session[:current_user].nil? && !article.nil? &&
+                       session[:current_user]['id'] == article.author.id
+
+    'd-none'
+  end
+
+  def article_image(article)
+    return article.image unless article.blank?
+
+    ''
+  end
+
+  def article_blank?(categories, most_voted)
+    if !most_voted.blank?
+      render partial: 'index', locals: { categories: categories, most_voted: most_voted }
+    else
+      render partial: 'no-article', locals: {}
+    end
+  end
+
+  def articles_by_category(article, category, count)
+    if !article.blank?
+      render partial: 'categories/article-category',
+             locals: { category: category, article: article, count: count }
+    else
+      render partial: 'categories/no-article-category', locals: { category: category, count: count }
+    end
+  end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,49 @@
+class Article < ApplicationRecord
+  validates :author_id, :image, presence: true
+  validates :title, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :text, presence: true, length: { minimum: 50, maximum: 1000 }
+
+  belongs_to :author, class_name: 'User'
+  has_many :votes, dependent: :destroy
+
+  has_many :article_categories, dependent: :destroy
+  has_many :categories, through: :article_categories, source: :category
+
+  def self.most_voted
+    if !Vote.all.blank?
+      top = joins(:votes).group(:article_id).count.max_by do |_k, v|
+        v
+      end[0]
+
+      Article.find(top)
+    else
+      Article.find(1)
+    end
+  end
+
+  def self.most_voted_by(user)
+    return nil if Article.find_by(author_id: user).blank?
+
+    user = User.find(user)
+    votes = user.articles.joins(:votes).group('articles.id')
+    top = votes.count.max_by do |_k, v|
+      v
+    end[0]
+
+    Article.find(top)
+  end
+
+  def self.most_recents
+    Article.order(created_at: :desc)
+  end
+
+  def self.search(title)
+    @articles = Article.where('lower(title) LIKE ?', "%#{title.downcase}%")
+    @articles = @articles.includes(:author, :article_categories, :categories).order(created_at: :desc)
+  end
+
+  def self.mine?(article_id, logged_user)
+    article = Article.find(article_id)
+    return true unless article.author.id != logged_user
+  end
+end
