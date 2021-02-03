696f682e038020832213bf98adeb52813680d29a
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-03 11:09:42 +0600

    login page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,63 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+          session[:current_user] = @user
+          redirect_to user_path(@user), notice: 'User is created successfully'
+        else
+            render new_user_path, notice: 'Something is wrong.'
+        end
+    end
+
+    def signin
+        @user = User.find_by(username: params[:username])
+        if !user.nil? 
+            session[:current_user] = @user
+            redirect_to root_path
+        else
+            redirect_to login_path, notice: 'No user is found'
+        end
+    end
+
+    def edit
+
+    end
+
+    def update 
+        if @user.update(user_params)
+            redirect_to user_path, notice: 'Updated successfully'
+        else
+            redirect_to edit_user_path(@user), notice: 'Cannot update'
+        end
+    end
+
+    def logout
+        reset_session
+        redirect_to root_path
+    end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :username)
+  end
+
+  def find_user
+    if params[:id].nil?
+      flash[:notice] = 'user is invalid'
+    else
+      @user = User.find(params[:id])
+    end
+  end
+
+  def signin_params
+    params.require(:user).permit(:username)
+  end
+
+  def user_loggedin?
+    redirect_to login_path if session[:current_user].nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,18 @@ Rails.application.routes.draw do
   post '/signin', to: 'users#signin'
   get '/suggestions', to: 'articles#suggestions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :show, :update, :index, :create, :edit] do
+    resources :articles, only: [:new, :show, :update, :edit, :create, :index] 
+  end
+
+  resource :votes, only: [:new, :edit] 
+
+  resources :articles, only: [:new, :show, :update, :edit, :create, :index]
+
+  get '/search', to: 'articles#search'
+
+  resources :categories, only: [:new, :edit, :index, :create, :update] do 
+    resources :articles, only: [:new, :show, :update, :edit, :create, :index]
+  end
 end
