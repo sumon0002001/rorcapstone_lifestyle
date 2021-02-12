f5cd66cfae4a33e11da1afe025debce6ab9d3e24
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-13 02:29:18 +0600

    Remove commented out codes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_name(params[:name])
-    # If the user exists
     if @user
-      # Save the user id inside the browser cookie. This is how we keep the user
-      # logged in when they navigate around our website.
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      # If user's login doesn't work, send them back to the login form.
       render :new
     end
   end
